05e6107fffcabe88d154a2aaa0d95699be36d418
JiukOn <78093035+JiukOn@users.noreply.github.com>
2026-01-07 17:00:52 -0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+/* Importação do CSS global do painel interativo */
+import './App.css'
+
+/* TÍTULOS BONITOS: RENDERIZAÇÃO DA ENGINE 2026 */
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
